49dcfe0a1fc51858dc3f30f57ff97b2009bfd804
MKG <mkgmanoj1976@hotmail.com>
2016-05-27 19:08:26 +0530

    It is initial commit

---
+++ app/src/main/java/com/example/mkg/gitexample/FItExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.mkg.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FItExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fit_example);
+    }
+}
